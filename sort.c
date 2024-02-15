3b5d7764997dfee9b8f653550a6d0ad58950df54
Bjorn <shurdha@bc.edu>
2024-02-15 15:31:54 -0500

    submitting a3

--- sort.c
+++ sort.c
@@ -3,10 +3,38 @@
 #include <string.h>
 #include <ctype.h>
 
-void usage();
-int is_valid_num(char *word);
-void swap(double *x, double *y);
-void sort(double *a, int length);
+void usage(){	
+	printf("sort [-r] num1 num2 ...\n");
+
+}
+	
+void swap(double *x, double *y){
+	if (x == NULL || y == NULL){
+		printf("sort [-r] num1 num2 ...\n");
+		
+	}
+	if (x!= y){
+		double stuf = *x;
+		*x = *y;
+		*y = stuf;
+	}
+}
+
+void sort(double *a, int length){
+	if (a == NULL) {
+		printf("sort [-r] num1 num2 ...\n");	
+		
+	}	
+	int i =1;
+	while (i<length){
+		int j=i;
+		while (j>0 && a[j-1] >a[j]){
+			swap(&a[j], &a[j-1]);
+			j = j-1;
+		}
+		i = i+1;
+	}
+}
 
 int is_valid_num(char *word)
 {
@@ -42,7 +70,45 @@ int is_valid_num(char *word)
     return 1;
 }
 
-int main(int argc, char *argv[])
-{
-    return 0;
+int main(int argc, char *argv[]){
+	if (argc < 2){
+		usage();
+	}
+
+	int reverse = 0;
+	int beginn = 1;
+
+	if (argc > 2 && strcmp(argv[1], "-r") == 0){
+		reverse = 1;
+		beginn = 2; 
+	}
+	if (argc - beginn < 1) {
+		usage();
+	}
+	int numboli = argc - beginn;
+	double *numbers = malloc(numboli * sizeof(double));
+
+	for (int i = 0; i < numboli; i++){
+		char *endptr;
+		numbers[i] = strtod(argv[beginn + i], &endptr);
+
+	if (*endptr != '\0'){
+		free(numbers);
+		usage();
+		}
+	}
+	sort(numbers, numboli);
+
+	if (reverse) {
+		for (int i = numboli - 1; i>=0; i--){
+			printf("%g\n", numbers [i]);
+		}
+	}
+	else {
+		for (int i=0; i< numboli; i++){
+			printf("%g\n",numbers[i]);
+		}
+	}
+	free(numbers);
+    	return 0;
 }
