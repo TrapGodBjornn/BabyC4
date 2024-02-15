51829ae67115935c92391d80641a55122436a269
Bjorn <shurdha@bc.edu>
2024-02-15 18:36:23 -0500

    submitting a3

--- sort.c
+++ sort.c
@@ -4,14 +4,13 @@
 #include <ctype.h>
 
 void usage(){	
-	printf("sort [-r] num1 num2 ...\n");
-
+	printf("usage: sort [-r] num1 num2 ...\n");
+        printf("      -r: reverse sort\n");
 }
 	
 void swap(double *x, double *y){
 	if (x == NULL || y == NULL){
-		printf("sort [-r] num1 num2 ...\n");
-		
+		usage();
 	}
 	if (x!= y){
 		double stuf = *x;
@@ -22,7 +21,7 @@ void swap(double *x, double *y){
 
 void sort(double *a, int length){
 	if (a == NULL) {
-		printf("sort [-r] num1 num2 ...\n");	
+		usage();
 		
 	}	
 	int i =1;
@@ -73,6 +72,7 @@ int is_valid_num(char *word)
 int main(int argc, char *argv[]){
 	if (argc < 2){
 		usage();
+		return 0;
 	}
 
 	int reverse = 0;
@@ -81,20 +81,41 @@ int main(int argc, char *argv[]){
 	if (argc > 2 && strcmp(argv[1], "-r") == 0){
 		reverse = 1;
 		beginn = 2; 
+
+	
 	}
 	if (argc - beginn < 1) {
 		usage();
+		return 0;
 	}
 	int numboli = argc - beginn;
 	double *numbers = malloc(numboli * sizeof(double));
 
+	if (numbers == NULL){
+		usage();
+		return 1;
+	}
+	
+
 	for (int i = 0; i < numboli; i++){
+		if (i==0 && strcmp(argv[beginn +i], "-r")==0){
+			usage();
+			return 0;
+		}
+
 		char *endptr;
+		if (!is_valid_num(argv[beginn + i])){
+			printf("%s is not a valid number\n", argv[beginn + i]);
+			free(numbers);
+			usage();
+			return 0;
+		}
 		numbers[i] = strtod(argv[beginn + i], &endptr);
 
 	if (*endptr != '\0'){
 		free(numbers);
 		usage();
+		return 0;
 		}
 	}
 	sort(numbers, numboli);
